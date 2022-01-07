62618714793d1ad7824f355ef0b97d941fe5881f
Wellington PIres <wellington.goncalves@live.com>
2022-01-07 10:57:17 -0300

    First commit

---
+++ src/main/java/br/com/furafila/credentialsapp/FuraFilaCredentialsAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.furafila.credentialsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuraFilaCredentialsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuraFilaCredentialsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/furafila/credentialsapp/FuraFilaCredentialsAppApplicationTests.java
@@ -0,0 +1,10 @@
+package br.com.furafila.credentialsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuraFilaCredentialsAppApplicationTests {
+
+
+}
